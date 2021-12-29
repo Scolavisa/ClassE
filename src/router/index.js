1e2d9945bc8eb25ad2fef0af0b336d3e136d23f5
Michael <michael@scolavisa.eu>
2021-12-29 14:50:48 +0100

    if not logged in, redirect to login

--- src/router/index.js
+++ src/router/index.js
@@ -1,6 +1,9 @@
 import { createRouter, createWebHistory } from "vue-router";
+import useAuth from "@/composables/UseAuth";
 import Home from "@/views/HomePage.vue";
 import Login from "@/views/LoginPage.vue";
+
+const { authenticated } = useAuth();
 
 const routes = [
     {
@@ -20,10 +23,22 @@ const router = createRouter({
     routes
 });
 
+
+/**
+ * if not logged in: redirect to login page
+ */
+router.beforeEach((to, from, next) => {
+    if (to.name !== 'login' && !authenticated.value) {
+        next({name: 'login'});
+    } else {
+        next();
+    }
+})
+
 /**
  * close nav bar after
  */
-router.beforeEach((from, to, next) => {
+router.beforeEach((to, from, next) => {
     const toggler = document.getElementById("navbar-toggler");
     const isExpanded = toggler?.getAttribute('aria-expanded')==='true';
     if (isExpanded) toggler.click();
