e9b3850b0e1a4f5e5b540e4926cddd2ee263ae2f
Michael <michael@scolavisa.eu>
2021-12-29 19:32:31 +0100

    no menu when not logged in

--- src/components/HeaderSection.vue
+++ src/components/HeaderSection.vue
@@ -11,45 +11,56 @@
                     <div class="d-inline d-sm-none"><h1>XS</h1></div>
                 </div>
                 <a v-else class="navbar-brand" href="#">CLASS-ED</a>
-                <button class="btn btn-primary" id="ocToggler" type="button" data-bs-toggle="offcanvas" data-bs-target="#mainMenu" aria-controls="mainMenu">
-                    <span class="navbar-toggler-icon"></span>
-                </button>
-                <div class="offcanvas offcanvas-start" tabindex="-1" id="mainMenu" aria-labelledby="mainMenuLabel">
-                    <div class="offcanvas-header">
-                        <h5 class="offcanvas-title" id="mainMenuLabel">Menu</h5>
-                        <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas" aria-label="Close"></button>
-                    </div>
-                    <div class="offcanvas-body">
-                        <div class="d-grid gap-2">
-                            <router-link to="/" class="btn btn-primary">
-                                <font-awesome-icon icon="home" />
-                                Home
-                            </router-link>
-                            <router-link v-if="authenticated" to="/calendarmonth" class="btn btn-primary">
-                                <font-awesome-icon icon="calendar" />
-                                Calendar
-                            </router-link>
+                <template v-if="authenticated">
+                    <button
+                        class="btn btn-primary"
+                        id="ocToggler"
+                        type="button"
+                        data-bs-toggle="offcanvas"
+                        data-bs-target="#mainMenu"
+                        aria-controls="mainMenu"
+                    >
+                        <span class="navbar-toggler-icon"></span>
+                    </button>
+                    <div class="offcanvas offcanvas-start" tabindex="-1" id="mainMenu" aria-labelledby="mainMenuLabel">
+                        <div class="offcanvas-header">
+                            <h5 class="offcanvas-title" id="mainMenuLabel">Menu</h5>
+                            <button type="button" class="btn-close text-reset" data-bs-dismiss="offcanvas"
+                                    aria-label="Close"></button>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div class="d-grid gap-2">
+                                <router-link to="/" class="btn btn-primary">
+                                    <font-awesome-icon icon="home"/>
+                                    Home
+                                </router-link>
+                                <router-link v-if="authenticated" to="/calendarmonth" class="btn btn-primary">
+                                    <font-awesome-icon icon="calendar"/>
+                                    Calendar
+                                </router-link>
+                            </div>
+                        </div>
+                        <div class="offcanvas-body">
+                            <div class="d-grid gap-2">
+                                <button v-if="authenticated" @click="logOut" class="btn btn-secondary" type="button">
+                                    <font-awesome-icon icon="sign-out-alt"/>
+                                    Logout
+                                </button>
+                            </div>
                         </div>
                     </div>
-                    <div class="offcanvas-body">
-                        <div class="d-grid gap-2">
-                            <button v-if="authenticated" @click="logOut" class="btn btn-secondary" type="button">
-                                <font-awesome-icon icon="sign-out-alt" />
-                                Logout
-                            </button>
-                        </div>
-                    </div>
-                </div>
+                </template>
             </div>
         </nav>
     </header>
 </template>
 
 <script setup>
-import { ref } from "vue";
-import { useRouter } from "vue-router";
+import {ref} from "vue";
+import {useRouter} from "vue-router";
 import useAuth from "../composables/UseAuth";
-const { authenticated, signOut } = useAuth();
+
+const {authenticated, signOut} = useAuth();
 const showBreakpoints = false;
 const router = useRouter();
 const error = ref('');
